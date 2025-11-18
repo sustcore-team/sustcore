02cd23a35f61637f4b3fd313f07c145f121375d4
theflysong <song_of_the_fly@163.com>
2025-11-18 10:43:04 +0800

    🦄 refactor(basec, std): Replant std headers
    Replanted standard headers from linux

--- third_party/include/std/limits.h
+++ third_party/include/std/limits.h
@@ -196,9 +196,4 @@ see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
    if the system has its own version of limits.h.  */
 
 #else /* not _GCC_LIMITS_H_ */
-
-#ifdef _GCC_NEXT_LIMITS_H
-#include_next <limits.h>		/* recurse down to the real one */
-#endif
-
 #endif /* not _GCC_LIMITS_H_ */
