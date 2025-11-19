e286168f7b06bd79b6459ee90384874c19672a07
theflysong <song_of_the_fly@163.com>
2025-11-19 11:33:56 +0800

    ✨ feat(makefile): update run.mk
    add architecture-specific arguments for qemu ; allow attached arguments in config.mk

--- script/run.mk
+++ script/run.mk
@@ -1,18 +1,26 @@
 # 权益之计
-qemu-args ?= -m size=256m,maxmem=256m \
+qemu-args-riscv64 ?= -m size=256m,maxmem=256m \
 			 -bios default \
 			 -serial stdio \
  			 -rtc base=localtime \
  			 -name "Sustcore" \
-			 -machine virt -d int \
+			 -machine virt \
 			 -kernel ./build/bin/kernel/sustcore.bin
 
-qemu-debug ?= -s -S
+qemu-attached-args-riscv64 ?=
 
-.PHONY: run
+qemu-debug-riscv64 ?= -s -S
+
+ifeq ($(architecture), riscv64)
+	qemu-args := $(qemu-args-riscv64)
+	qemu-attached-args := $(qemu-attached-args-riscv64)
+	qemu-debug := $(qemu-debug-riscv64)
+endif
+
+.PHONY: run-risc
 run:
 	$(qemu) $(qemu-args)
 
 .PHONY: run_dbg
 run_dbg:
-	$(qemu) $(qemu-args) $(qemu-debug)
+	$(qemu) $(qemu-args) $(qemu-attached-args) $(qemu-debug)
