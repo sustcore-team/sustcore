dc5660db388fa7376e9f034cf8f3da6ef7cb7d16
theflysong <song_of_the_fly@163.com>
2025-12-07 14:56:50 +0800

    ✨ feat(libkmod): Keep pid-cap relationship
    Keep a hash table to maintain pid-cap relationship

--- libs/kmod/syscall.c
+++ libs/kmod/syscall.c
@@ -11,6 +11,7 @@
 
 #include <kmod/syscall.h>
 #include <sus/syscall.h>
+#include <sus/list_helper.h>
 #include <startup.h>
 
 umb_t syscall(int sysno, umb_t arg0, umb_t arg1, umb_t arg2, umb_t arg3, umb_t arg4, umb_t arg5, umb_t arg6)
@@ -81,15 +82,66 @@ int get_current_pid(void) {
 	return get_pid(pcb_cap);
 }
 
+// 我们通过一个HashMap来维护pid与cap的对应关系
+// 一般来说pid是线性增长的, 所以这个HashTable可以视作M个链表
+
+typedef struct ProcCapNodeStruct {
+	int pid;
+	CapPtr cap;
+	struct ProcCapNodeStruct *next;
+	struct ProcCapNodeStruct *prev;
+} ProcCapNode;
+
+#define PROC_CAP_TABLE_SIZE (1 << 8)
+#define HASH_PID(pid) ((pid) & 0xFF)
+
+static ProcCapNode *proc_cap_table_head[PROC_CAP_TABLE_SIZE] = {};
+static ProcCapNode *proc_cap_table_tail[PROC_CAP_TABLE_SIZE] = {};
+
+#define PROC_CAP_LIST(pid) proc_cap_table_head[HASH_PID(pid)], proc_cap_table_tail[HASH_PID(pid)], next, prev
+
+CapPtr get_proc_cap(int pid) {
+	ProcCapNode *node;
+	foreach_list(node, PROC_CAP_LIST(pid)) {
+		if (node->pid == pid) {
+			return node->cap;
+		}
+	}
+	// 未找到
+	CapPtr null_cap;
+	null_cap.val = 0;
+	return null_cap;
+}
+
+void insert_proc_cap(int pid, CapPtr cap) {
+	// 首先先判断是否存在
+	// 已存在, 更新
+	ProcCapNode *node;
+	foreach_list(node, PROC_CAP_LIST(pid)) {
+		if (node->pid == pid) {
+			node->cap = cap;
+			return;
+		}
+	}
+	// 不存在, 插入新节点
+	ProcCapNode *node = (ProcCapNode *)kmalloc(sizeof(ProcCapNode));
+	node->pid = pid;
+	node->cap = cap;
+	list_push_back(node, PROC_CAP_LIST(pid));
+}
+
 int fork(void) {
 	CapPtr cap;
 	int pid;
 	cap.val = syscall_2(SYS_FORK, pcb_cap.val, 0, 0, 0, 0, 0, 0, (umb_t *)&pid);
-	// TODO: 维护一个列表
+	// 将cap与pid插入表中
+	insert_proc_cap(pid, cap);
 	// 将pid与cap对应起来
 	if (pid == 0){
 		// 子进程, 更新pcb_cap
 		pcb_cap = cap;
+		// 子进程也要插入自己的cap
+		insert_proc_cap(get_current_pid(), cap);
 	}
 	return pid;
 }
---
+++ libs/kmod/syscall.h
@@ -0,0 +1,38 @@
+/**
+ * @file syscall.h
+ * @author theflysong (song_of_the_fly@163.com)
+ * @brief 系统调用相关(libkmod特定函数)
+ * @version alpha-1.0.0
+ * @date 2025-12-07
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#pragma once
+
+#include <sus/capability.h>
+
+/**
+ * @brief 获得能力对应的进程ID
+ * 
+ * @param cap 进程能力
+ * @return int 进程ID
+ */
+int get_pid(CapPtr cap);
+
+/**
+ * @brief 获得指定pid的进程能力
+ * 
+ * @param pid 进程ID
+ * @return CapPtr 进程能力, 未找到则val为0
+ */
+CapPtr get_proc_cap(int pid);
+
+/**
+ * @brief 插入进程能力到哈希表
+ * 
+ * @param pid 进程ID
+ * @param cap 进程能力
+ */
+void insert_proc_cap(int pid, CapPtr cap);
