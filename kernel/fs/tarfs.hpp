ac48ea74e73d915236503421f8afa670e414e948
hyj0824 <2417179808@qq.com>
2026-02-14 03:52:47 +0800

    ✨ feat(tarfs): 优化文件读取逻辑，实现所需接口

--- kernel/fs/tarfs.hpp
+++ kernel/fs/tarfs.hpp
@@ -4,9 +4,13 @@
  * @brief Tar Based initramfs
  * @version alpha-1.0.0
  * @date 2026-02-13
- * 
+ *
+ * 生命周期由 Dentry 树管理，INode 和 Dentry 保证一一对应；
+ * 所以 TarNode 糅合了 Dentry 与 INode，
+ * 负责析构 File/Directory 视图
+ *
  * @copyright Copyright (c) 2026
- * 
+ *
  */
 
 #pragma once
@@ -14,14 +18,32 @@
 #include <device/block.h>
 #include <stdio.h>
 #include <string.h>
+#include <sus/list.h>
+#include <sus/mstring.h>
 #include <vfs/ops.h>
 
 #include <cassert>
 #include <cstdint>
 
+namespace TarFS {
+
 using uint8_t = unsigned char;
 
 static constexpr size_t BLOCK_SIZE = 512;
+
+/**
+ * @brief 判断pfx是否是str的前缀
+ */
+bool _prefix(const char *str, const char *pfx) {
+    while (*pfx != 0) {
+        if (*str != *pfx) {
+            return false;
+        }
+        str++;
+        pfx++;
+    }
+    return true;
+}
 
 template <typename T>
 size_t parse_octal(const T &field) {
@@ -99,17 +121,63 @@ union TarBlock {
 };
 
 class TarFile : public IFile {
+private:
+    TarNode *node_;
+    const uint8_t *ptr_;
+
 public:
+    TarFile(TarNode *node) : node_(node), ptr_(node_->data()) {}
+
     FSOptional<size_t> read(void *buf, size_t len) override {
-        return FSErrCode::NOT_SUPPORTED;
+        size_t file_size = parse_octal(node_->header_->header.size);
+        size_t offset    = ptr_ - node_->data();
+        if (offset >= file_size) {
+            return 0;  // 已经读到文件末尾了，读取长度为0
+        }
+        size_t to_read = std::min(len, file_size - offset);
+        memcpy(buf, ptr_, to_read);
+        ptr_ += to_read;
+        return to_read;
     }
 
     FSOptional<size_t> write(const void *buf, size_t len) override {
         return FSErrCode::NOT_SUPPORTED;
     }
 
+    // ! need review (AI code)
     FSOptional<off_t> seek(off_t offset, SeekWhence whence) override {
-        return FSErrCode::NOT_SUPPORTED;
+        // off_t 是有符号的
+        size_t file_size = parse_octal(node_->header_->header.size);
+        size_t new_offset;
+        switch (whence) {
+            case SeekWhence::SET:
+                if (offset < 0) {
+                    return FSErrCode::INVALID_PARAM;
+                }
+                new_offset = static_cast<size_t>(offset);
+                break;
+            case SeekWhence::CUR:
+                if (offset < 0 &&
+                    static_cast<size_t>(-offset) > ptr_ - node_->data())
+                {
+                    return FSErrCode::INVALID_PARAM;
+                }
+                new_offset = static_cast<size_t>(ptr_ - node_->data()) +
+                             static_cast<size_t>(offset);
+                break;
+            case SeekWhence::END:
+                if (offset > 0 || static_cast<size_t>(-offset) > file_size) {
+                    return FSErrCode::INVALID_PARAM;
+                }
+                new_offset = file_size + static_cast<size_t>(offset);
+                break;
+            default: return FSErrCode::INVALID_PARAM;
+        }
+        if (new_offset > file_size) {
+            return FSErrCode::INVALID_PARAM;
+        }
+        ptr_ = node_->data() + new_offset;
+        return static_cast<off_t>(new_offset);
     }
 
     FSErrCode sync(void) override {
@@ -118,9 +186,46 @@ public:
 };
 
 class TarDirectory : public IDirectory {
+private:
+    TarNode *node_;
+
 public:
+    TarDirectory(TarNode *node) : node_(node) {}
+
     FSOptional<IDentry *> lookup(const char *name) override {
-        return FSErrCode::NOT_SUPPORTED;
+        util::string_builder path{node_->header_->header.name};
+        util::string pfx = path.build();
+        if (path[path.size() - 1] != '/')
+            path.append('/');
+        path.append(name);
+        util::string full_path = path.build();
+
+        for (auto p : node_->children_) {
+            if (strcmp(p->header_->header.name, full_path.c_str()) == 0) {
+                return p;
+            }
+        }
+
+        IDentry *ret = nullptr;
+
+        for (auto p = node_->data();;) {
+            // 当 pfx 不再是 tar块 的前缀时，说明已经超出目录范围了
+            const TarBlock *block = reinterpret_cast<const TarBlock *>(p);
+            if (!_prefix(block->header.name, pfx.c_str()))
+                break;
+            if (!block->is_header())
+                return FSErrCode::UNKNOWN_ERROR;
+
+            node_->children_.emplace_back(p);
+            if (full_path == block->header.name) {
+                ret = node_->children_.back();
+            }
+
+            size_t file_size   = parse_octal(block->header.size);
+            size_t file_block  = (file_size + BLOCK_SIZE - 1) / BLOCK_SIZE;
+            p                 += BLOCK_SIZE * (file_block + 1);
+        }
+        return FSErrCode::INVALID_PARAM;  // 未找到对应目录项
     }
 
     FSOptional<IDentry *> create(const char *name, bool is_dir) override {
@@ -132,13 +237,63 @@ public:
     }
 };
 
-class TarDentry : public IDentry {
+class TarNode : public IINode, public IDentry {
 private:
-    TarINode *inode_;
+    // 理论上，多个 Dentry 可能指向同一个 INode
+    // 但在 tarfs 中，每个 Dentry 都对应一个 INode
+    // 这样可以简化生命期管理，增加缓存命中
+    // 否则就得上引用计数了
+    bool is_dir_;
+    const TarBlock *header_;
+    union {
+        TarDirectory *dir;
+        TarFile *file;
+        void *raw;
+    } view_{};
+    util::ArrayList<TarNode *> children_{};
+
+    const uint8_t *data() {
+        return reinterpret_cast<const uint8_t *>(header_) + BLOCK_SIZE;
+    }
 
 public:
+    TarNode(const uint8_t *header) {
+        header_ = reinterpret_cast<const TarBlock *>(header);
+        is_dir_ = header_->header.typeflag[0] == '5';
+    }
+
+    ~TarNode() noexcept {
+        delete view_.raw;
+    }
+
+    // IINode
+
+    FSOptional<IDirectory *> as_directory(void) override {
+        if (!is_dir_)
+            return FSErrCode::INVALID_PARAM;
+        if (!view_.dir) {
+            view_.dir = new TarDirectory{this};
+        }
+        return view_.dir;
+    }
+
+    FSOptional<IFile *> as_file(void) override {
+        if (is_dir_)
+            return FSErrCode::INVALID_PARAM;
+        if (!view_.file) {
+            view_.file = new TarFile{this};
+        }
+        return view_.file;
+    }
+
+    FSOptional<IMetadata *> metadata(void) override {
+        return FSErrCode::NOT_SUPPORTED;
+    }
+
+    // IDentry
+
     FSOptional<const char *> name(void) override {
-        return inode_->header_->header.name;
+        return header_->header.name;
     }
 
     FSErrCode remove(void) override {
@@ -150,60 +305,19 @@ public:
     }
 
     FSOptional<IINode *> inode(void) override {
-        return inode_;
-    }
-};
-
-// INode 就是 header
-class TarINode : public IINode {
-private:
-    const TarBlock *header_;
-    const uint8_t *data_;  // 指向 header 之后的数据
-    union {
-        TarDirectory *dir;
-        TarFile *file;
-    } view_{};
-
-public:
-    TarINode(const uint8_t *header, const uint8_t *data) {
-        header_ = reinterpret_cast<const TarBlock *>(header);
-        data_   = data;
+        return dynamic_cast<IINode *>(this);
     }
 
-    FSOptional<IDirectory *> as_directory(void) override {
-        if (header_->header.typeflag[0] != '5') {
-            return FSErrCode::INVALID_PARAM;  // 不是目录
-        }
-        if (!view_.dir) {
-            view_.dir = new TarDirectory();
-        }
-        return view_.dir;
-    }
-
-    FSOptional<IFile *> as_file(void) override {
-        if (header_->header.typeflag[0] != '0' &&
-            header_->header.typeflag[0] != '\0')
-        {
-            return FSErrCode::INVALID_PARAM;  // 不是普通文件
-        }
-        if (!view_.file) {
-            view_.file = new TarFile();
-        }
-        return view_.file;
-    }
-
-    FSOptional<IMetadata *> metadata(void) override {
-        return FSErrCode::NOT_SUPPORTED;
-    }
-    friend class TarDentry;
+    friend class TarFile;
+    friend class TarDirectory;
 };
 
 class TarSuperblock : public ISuperblock {
 private:
-    const uint8_t *data_;  // 只读数据
-    size_t size_;
+    const uint8_t *data_;  // 只读数据段
+    const size_t size_;
     TarFSDriver *fs_;
-    TarINode *root_{};
+    TarNode *root_{};
 
 public:
     TarSuperblock(const uint8_t *data, size_t size, TarFSDriver *fs)
@@ -212,6 +326,7 @@ public:
     ~TarSuperblock() override {
         delete[] data_;
         delete root_;
+        // 触发全部析构
     }
 
     IFsDriver *fs() override {
@@ -223,14 +338,22 @@ public:
     }
 
     // tarfs 的根目录就是 tar 包中的第一个 header
-    // ! unsafe 不能从外部 delete INode
+    // ! unsafe 不能从外部 delete root_
+    // 否则除了数据指针，tarfs 对象的所有指针都会失效
     FSOptional<IINode *> root() override {
         // 返回根目录 inode
         if (!root_) {
-            root_ = new TarINode(data_, data_ + BLOCK_SIZE);
+            root_ = new TarNode(data_);
         }
         return root_;
     }
+
+    // FSOptional<IDentry *> root_d() {
+    //     if (!root_) {
+    //         root_ = new TarDentry(data_);
+    //     }
+    //     return root_;
+    // }
 
     FSOptional<IMetadata *> metadata() override {
         return FSErrCode::NOT_SUPPORTED;
@@ -258,7 +381,7 @@ private:
             } else {
                 if (block->is_empty()) {
                     offset += BLOCK_SIZE;
-                    // 允许文件有空块
+                    // 允许有空块
                 } else {
                     return false;
                 }
@@ -307,3 +430,5 @@ public:
         return FSErrCode::SUCCESS;
     }
 };
+
+};  // namespace TarFS
